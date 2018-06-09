2b1947c1db35cf6b88dea03c7590126635377d8b
marcussvsc <marcuss.vsc@gmail.com>
2018-06-09 17:05:38 -0300

    Tudo funcionando, aplicar boostrap.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
+  def nav_link(title, path)
+    classValue = current_page?(path) ? 'active' : ''
 
+    content_tag(:li, class: classValue) do
+      link_to title, path
+    end
+  end
   def title(text)
     content_tag(:div, class: "page-header") do
       content_tag(:h1) do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180607183658) do
+ActiveRecord::Schema.define(version: 20180609192429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180607183658) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
